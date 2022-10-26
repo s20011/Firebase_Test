3d86f8c60e447057ed474d972709ac5cefb5bf58
s20011 <s20011@std.it-college.ac.jp>
2022-10-26 14:41:05 +0900

    Initial commit

---
+++ app/src/main/java/com/example/firebase_test/MainActivity.kt
@@ -0,0 +1,136 @@
+package com.example.firebase_test
+
+import android.os.Bundle
+import android.renderscript.Sampler.Value
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.firebase_test.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        Dbreadwrite("s20041",
+            "zayasuseiya",
+            "s20041@std.it-college.ac.jp")
+    }
+
+    fun Dbreadwrite(userId: String, name: String, email: String){
+        val user = User(name, email)
+
+        //データベースのインスタンス取得
+        val database = Firebase.database.reference
+
+        //データベースへの書き込み
+        database.child("users").child(userId).child("username").setValue(name)
+        //データベースへの書き込み
+        /*val myRef = database.getReference("message")
+        myRef.setValue("Hello, World!")*/
+
+
+        /*val postListener = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val post = snapshot.value
+                Log.d("MAINACTIVITY", post.toString())
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("MainActivity", "Failed to read value.", error.toException())
+            }
+        }*/
+
+
+
+
+
+        //データベースを読み込む
+        database.child("users").child(userId)//.child("username")
+            .addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(datasnapshot: DataSnapshot) {
+                val value = datasnapshot.getValue(User::class.java)
+
+                //ノードの名前
+                val child = datasnapshot.key.toString()
+
+                Log.d("MainActivity", "Value is: $value")
+                Log.d("MainActivity", "Datachildren: $child")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("MainActivity", "Failed to read value.", error.toException())
+            }
+        })
+
+        database.child("users").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val list = mutableListOf<String>()
+                //子ノードを文字列として取得
+                snapshot.children.forEach { list.add(it.key.toString()) }
+                Log.d("MainActivity", "childd: $list")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
+
+
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
